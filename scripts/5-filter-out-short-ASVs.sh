2358334d38934412396b70f78e15d3f059c852cd
Gregory Putzel <grp2009@med.cornell.edu>
2021-09-22 12:04:43 -0400

    Make visualizations of length-filtered representative sequences and ASV table

--- scripts/5-filter-out-short-ASVs.sh
+++ scripts/5-filter-out-short-ASVs.sh
@@ -10,4 +10,12 @@ qiime feature-table filter-features \
     --i-table artifacts/table.qza \
     --m-metadata-file artifacts/rep-seqs.qza \
     --p-where 'length(sequence) > 300' \
-    --o-filtered-table artifacts/table-length-filtered.qz
+    --o-filtered-table artifacts/table-length-filtered.qza
+
+qiime feature-table tabulate-seqs \
+    --i-data artifacts/rep-seqs-length-filtered.qza \
+    --o-visualization visualizations/rep-seqs-length-filtered.qzv
+
+qiime feature-table summarize \
+    --i-table artifacts/table-length-filtered.qza \
+    --o-visualization visualizations/table-length-filtered.qzv

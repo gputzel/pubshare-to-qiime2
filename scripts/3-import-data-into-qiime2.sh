33335c30e8ba7c3c5fcc49b8c7928b9d313ff38a
Gregory Putzel <grp2009@med.cornell.edu>
2021-09-22 11:48:59 -0400

    Generate visualization for importing FASTQs

--- scripts/3-import-data-into-qiime2.sh
+++ scripts/3-import-data-into-qiime2.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 [ ! -d "artifacts" ] && mkdir artifacts
-qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path sample-manifest.txt --input-format PairedEndFastqManifestPhred33V2 --output-path artifacts/demux-paired-end.qza
+qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
+    --input-path sample-manifest.txt \
+    --input-format PairedEndFastqManifestPhred33V2 \
+    --output-path artifacts/demux-paired-end.qza
+
+[ ! -d "visualizations" ] && mkdir visualizations
+#Visualization
+qiime demux summarize --i-data artifacts/demux-paired-end.qza \
+    --o-visualization visualizations/demux-paired-end.qzv \
+    --verbose
